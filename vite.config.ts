badcbb758edfdc8bf1a9adc9d4dba7b7a68be5b5
Jose Antonio Albértuz <josealbertuz@gmail.com>
2022-12-10 11:05:42 +0100

    styles: add reset and base styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +21,8 @@ export default defineConfig({
       "components": "/src/components",
       "hooks": "/src/hooks",
       "assets": "/src/assets",
-      "test": "/src/test"
+      "test": "/src/test",
+      "styles": "/src/styles"
     },
   },
 });
