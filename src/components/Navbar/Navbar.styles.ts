32fbfea5be4d580f16e6441ecfb7d670c30faa45
Jose Antonio Albértuz <josealbertuz@gmail.com>
2022-12-18 13:09:02 +0100

    feat(wip): navbar component

---
+++ src/components/Navbar/Navbar.styles.ts
@@ -0,0 +1,22 @@
+import { styled } from "@stitches/react";
+
+export const NavbarRoot = styled("nav", {
+    display: 'flex',
+    justifyContent: 'space-between'
+});
+
+export const Leading = styled("div", {
+    marginRight: 'auto'
+});
+
+export const Trailing = styled("div", {
+    marginLeft: 'auto'
+});
+
+export const MenuList = styled('ul', {
+    
+})
+
+export const MenuListItem = styled('li', {
+
+})
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavbarRoot } from './Navbar.styles'
+
+type NavbarProps = {
+    leading: JSX.Element
+    trailing: JSX.Element
+}
+
+export const Navbar = ({leading, trailing}: NavbarProps) => {
+  return (
+    <NavbarRoot>
+        {}
+    </NavbarRoot>
+  )
+}
